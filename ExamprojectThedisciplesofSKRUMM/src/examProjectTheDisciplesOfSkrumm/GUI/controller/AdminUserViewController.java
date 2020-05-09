af19d7517005286a3412c3a26b3bdd2708b9e200
Lumby98 <lumby89@gmail.com>
2020-05-09 13:26:08 +0200

    updatedAdminView

---
+++ ExamprojectThedisciplesofSKRUMM/src/examProjectTheDisciplesOfSkrumm/GUI/controller/AdminUserViewController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package examProjectTheDisciplesOfSkrumm.GUI.controller;
+
+import com.jfoenix.controls.JFXButton;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author lumby
+ */
+public class AdminUserViewController implements Initializable
+{
+
+    @FXML
+    private JFXButton createUserBtn;
+    @FXML
+    private JFXButton editBtn;
+    @FXML
+    private JFXButton deleteUser;
+    @FXML
+    private JFXButton adminBtn;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        // TODO
+    }    
+
+    @FXML
+    private void handleCreateUser(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void handleEditUser(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void handleDeleteUser(ActionEvent event)
+    {
+    }
+
+    @FXML
+    private void handleMakeAdmin(ActionEvent event)
+    {
+    }
+    
+}
