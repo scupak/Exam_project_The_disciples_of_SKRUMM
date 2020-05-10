80087c58ea52dc72804c371a25eb2c28c198102b
Lumby98 <lumby89@gmail.com>
2020-05-10 13:03:10 +0200

    it is now posible to enter the new adminViews

--- ExamprojectThedisciplesofSKRUMM/src/examProjectTheDisciplesOfSkrumm/GUI/controller/AdminClientsAndProjectsController.java
+++ ExamprojectThedisciplesofSKRUMM/src/examProjectTheDisciplesOfSkrumm/GUI/controller/AdminClientsAndProjectsController.java
@@ -6,12 +6,19 @@
 package examProjectTheDisciplesOfSkrumm.GUI.controller;
 
 import com.jfoenix.controls.JFXButton;
+import examProjectTheDisciplesOfSkrumm.BE.Client;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,7 +29,7 @@ public class AdminClientsAndProjectsController implements Initializable
 {
 
     @FXML
-    private TableView<?> clientTableView;
+    private TableView<Client> clientTableView;
     @FXML
     private TableView<?> projectTableView;
     @FXML
@@ -37,6 +44,8 @@ public class AdminClientsAndProjectsController implements Initializable
     private JFXButton editProjectBtn;
     @FXML
     private JFXButton deleteProjectBtn;
+    @FXML
+    private JFXButton backBtn;
 
     /**
      * Initializes the controller class.
@@ -48,13 +57,23 @@ public class AdminClientsAndProjectsController implements Initializable
     }    
 
     @FXML
-    private void handleCreateClient(ActionEvent event)
+    private void handleCreateClient(ActionEvent event) throws IOException
     {
+     FXMLLoader loader = new FXMLLoader(getClass().
+                getResource("/examProjectTheDisciplesOfSkrumm/GUI/view/AddClient.fxml"));
+        Parent root = loader.load();
+        AddClientController controller = loader.getController();
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setTitle("Create Client");
+        stage.show();   
     }
 
     @FXML
     private void handleEditClient(ActionEvent event)
     {
+        
     }
 
     @FXML
@@ -75,6 +94,25 @@ public class AdminClientsAndProjectsController implements Initializable
     @FXML
     private void handleDeleteProject(ActionEvent event)
     {
+    }
+
+    @FXML
+    private void handleBack(ActionEvent event) throws IOException
+    {
+        Stage adminClientsAndProjectsView = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        FXMLLoader loader = new FXMLLoader(getClass().
+                getResource("/examProjectTheDisciplesOfSkrumm/GUI/view/AdminMainView.fxml"));
+        Parent root = loader.load();
+        AdminMainViewController controller = loader.getController();
+
+        Stage stage = new Stage();
+        stage.setScene(new Scene(root));
+        stage.setMinHeight(523);
+        stage.setMinWidth(721);
+        stage.setTitle("Main Menu");
+        stage.show();
+        adminClientsAndProjectsView.close();
     }
     
 }
