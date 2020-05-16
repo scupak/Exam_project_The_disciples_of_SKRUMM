a28d7d6836e1da7ee94c703f78d42e3178e18b26
kasp608b <54952896+kasp608b@users.noreply.github.com>
2020-05-16 16:08:04 +0200

    Added multithreading
    Added executor service multithreading to the taskview

--- ExamprojectThedisciplesofSKRUMM/src/examProjectTheDisciplesOfSkrumm/GUI/controller/TaskViewController.java
+++ ExamprojectThedisciplesofSKRUMM/src/examProjectTheDisciplesOfSkrumm/GUI/controller/TaskViewController.java
@@ -576,25 +576,7 @@ public class TaskViewController implements Initializable
             });
         });
         
-        try 
-        {
-            System.out.println("attempt to shutdown executor");
-            executor.shutdown();
-            executor.awaitTermination(5, TimeUnit.SECONDS);
-        }
-        catch (InterruptedException e) 
-        {
-            System.err.println("tasks interrupted");
-        }
-        finally 
-        {
-            if (!executor.isTerminated()) 
-            {
-                System.err.println("cancel non-finished tasks");
-            }
-            executor.shutdownNow();
-            System.out.println("shutdown finished");
-        }
+        executor.shutdown();
     }
     
     @FXML
